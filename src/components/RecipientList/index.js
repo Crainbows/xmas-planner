5c56ad1c5a8bd2972aca01e1a171647734208f28
Oliver Balmford <oli@oli.codes>
2020-02-05 15:39:48 +0000

    Move recipientCard into new folder structure

--- src/components/RecipientList/index.js
+++ src/components/RecipientList/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import RecipientCard from "../recipientCard"
+import RecipientCard from "../RecipientCard"
 import './RecipientList.scss';
 
 class RecipientList extends Component {
