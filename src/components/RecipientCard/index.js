8cd9a6a46a36381d859fb7f3f5ffe66206413843
Oliver Balmford <oli@oli.codes>
2020-02-06 23:10:47 +0000

    WIP expanding recipient cards

--- src/components/RecipientCard/index.js
+++ src/components/RecipientCard/index.js
@@ -5,17 +5,29 @@ import './recipientCard.scss';
 // import { connect } from "react-redux";
 
 class RecipientCard extends Component{
-    contructor(props) {
-        // super(props);
-        this.setState({});
+    constructor(props) {
+        super(props);
+        this.state = {expanded: false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => {
+            return {expanded: !this.state.expanded}
+        });
     }
 
     render() {
+        let classes = this.state.expanded ? "recipient-card expanding-card expanded" : "recipient-card expanding-card";
+        let percentage = Math.random() * 100;
         return (
-            <div className="recipient-card">
+            <div className={classes} onClick={this.handleClick}>
                 <div className="recipient-name">{this.props.recipient.name}</div>
                 <div>{this.props.recipient.budget}</div>
-                <div className="progress-value"></div>
+                <div className="progress-container" style={{maxWidth: `${percentage}%`}}>
+                    <div className="progress-value"></div>
+                </div>
+                <div className="expandable">TEST TEXT<br />TEST TEXT<br />TEST TEXT<br />TEST TEXT<br />TEST TEXT<br /></div>
             </div>
         )
     }
