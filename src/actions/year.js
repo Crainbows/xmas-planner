ea316eab14683486680cc67788c6c3fb87cb62b8
Oliver Balmford <oli@oli.codes>
2020-02-06 22:15:54 +0000

    Add year changing to TopBar

---
+++ src/actions/year.js
@@ -0,0 +1,21 @@
+const INCREMENT_YEAR = 'INCREMENT_YEAR'
+const DECREMENT_YEAR = 'DECREMENT_YEAR'
+
+const incrementYear = () => {
+    return {
+        type: INCREMENT_YEAR,
+    };
+}
+
+const decrementYear = () => {
+    return {
+        type: DECREMENT_YEAR,
+    };
+}
+
+export {
+    INCREMENT_YEAR,
+    DECREMENT_YEAR,
+    incrementYear,
+    decrementYear,
+}
