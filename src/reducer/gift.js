42646c462093258cb53f5e5003d80e5b155ff591
Oliver Balmford <oli@oli.codes>
2020-02-09 19:26:00 +0000

    Add gift actions and reducers

---
+++ src/reducer/gift.js
@@ -0,0 +1,21 @@
+import {UPDATE_GIFT, ADD_GIFT} from '../actions/gift';
+
+export default(state = [], payload) => {
+    switch (payload.type) {
+        case UPDATE_GIFT:
+        case ADD_GIFT:
+            return {...state,
+                    [payload.uuid]: {
+                        recipientuuid: payload.recipientid,
+                        name: payload.name,
+                        price: payload.price,
+                        isPurchased: payload.isPurchased,
+                        isDelivered: payload.isDelivered,
+                        isWrapped: payload.isWrapped,
+                        isMade: payload.isMade,
+                    }
+                };
+        default:
+            return state;
+    }
+};
