dede2bfe12b6a2d5a0272cde6d44643a8f554971
Oliver Balmford <oli@oli.codes>
2020-02-09 19:26:00 +0000

    Add gift actions and reducers

--- src/actions/gift.js
+++ src/actions/gift.js
@@ -1,19 +1,41 @@
 import {CreateUUID} from '../utils/unique';
 
 const ADD_GIFT = 'ADD_GIFT'
+const UPDATE_GIFT = 'UPDATE_GIFT'
 
-const addNewGift = (gift) => {
+const addGift = (gift) => {
     console.log('adding item:', gift.name);
     return {
-        type: 'add',
-        giftid: "gift:"+CreateUUID(),
-        recipientid: gift.recipientid,
+        type: ADD_GIFT,
+        uuid: CreateUUID(),
+        recipientuuid: gift.recipientid,
         name: gift.name,
-        cost: gift.cost,
+        price: gift.price,
+        isPurchased: false,
+        isDelivered: false,
+        isWrapped: false,
+        isMade: false,
     };
-  }
+}
+
+const updateGift = (gift) => {
+    console.log('updating item:', gift.name);
+    return {
+        type: UPDATE_GIFT,
+        uuid: CreateUUID(),
+        recipientuuid: gift.recipientid,
+        name: gift.name,
+        price: gift.price,
+        isPurchased: gift.isPurchased,
+        isDelivered: gift.isDelivered,
+        isWrapped: gift.isWrapped,
+        isMade: gift.isMade,
+    };
+}
 
 export {
     ADD_GIFT,
-    addNewGift,
+    UPDATE_GIFT,
+    addGift,
+    updateGift,
 }
