86fb737bcfac694df416232075ad1bf44833b9b6
Oliver Balmford <oli@oli.codes>
2020-03-04 23:32:26 +0000

    Add blueprintjs and top level restyling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Core from './components/core';
-import './App.sass';
+// import './App.sass';
+import AppTopBar from './components/AppTopBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="logo">
-          Xmas Planner
-        </div>
-        <button>Account</button>
-      </header>
+    <div className="bp3-dark">
+      <AppTopBar></AppTopBar>
       <Core></Core>
     </div>
   );
